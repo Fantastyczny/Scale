f40c944422bd443b1a20d8a29f807b37a8c1dfde
Daniel Lister <dan.lister@gmail.com>
2011-09-21 14:55:30 -0400

    added Main.java
    Change-Id: I9b2033c61aed18464faca66dc44ffec6b026cb38

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author daniel
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
